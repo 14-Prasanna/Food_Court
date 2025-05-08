cd6b2e0082ed1b658bab689dff45940746e5994c
Prasanna venkatesh <168177926+14-Prasanna@users.noreply.github.com>
2025-05-08 21:05:59 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,47 +3,8 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { componentTagger } from 'lovable-tagger';
 
-const BASE_URL = "https://server-food-court.onrender.com";
-
 export default defineConfig(({ mode }) => ({
-  base: '/Food_Court/', 
-  server: {
-    host: '::',
-    port: 8080,
-    proxy: {
-      '/send-otp': {
-        target: BASE_URL,
-        changeOrigin: true,
-       
-      },
-      '/verify-otp': {
-        target: BASE_URL,
-        changeOrigin: true,
-        
-      },
-      '/check-user': {
-        target: BASE_URL,
-        changeOrigin: true,
-       
-      },
-      '/menu-items': {
-        target: BASE_URL,
-        changeOrigin: true,
-      
-      },
-      '/socket.io': {
-        target: BASE_URL,
-        ws: true,
-        changeOrigin: true,
-      
-      },
-      '^/customer': {
-        target: BASE_URL,
-        changeOrigin: true,
-       
-      },
-    },
-  },
+  base: '/Food_Court/',
   plugins: [
     react(),
     mode === 'development' && componentTagger(),
